d6655ad5ab5db4b98645d4ac1116535bdce0eea5
michael-zahgar <michaelzahgar@gmail.com>
2023-09-18 14:50:31 +0300

    new updates on css

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      console.log(entry)
       if (entry.isIntersecting) {
         entry.target.classList.add('show');
       } else {
