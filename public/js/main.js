3e96a0ace22b6905fef81fe44d1086cbd07f15f6
michael-zahgar <michaelzahgar@gmail.com>
2023-09-20 14:14:03 +0300

    new Update with fixed book

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
+// popover bootstrap on hover
+document.addEventListener('DOMContentLoaded' , function(){
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+})
 
 
 // On Scroll Animation
